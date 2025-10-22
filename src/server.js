cf36dc684e6424c82fbd190fe88755f2416dae86
GDC-Reis <gustavodonisete11@gmail.com>
2025-10-21 22:30:14 -0300

    :construction: Utilizando HTTP status code

--- src/server.js
+++ src/server.js
@@ -20,10 +20,10 @@ const server = http.createServer((req, res) => {
       email: 'johndoe@example.com'
     })
 
-    return res.end('Criação de Tarefas')
+    return res.writeHead(201).end()
   }
   
-  return res.end('Hello World')
+  return res.writeHead(404).end()
 })
 
 server.listen(3333)
