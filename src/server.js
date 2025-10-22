09fb6c5ee26035a5928fdcf9d7aefc79be8e3425
GDC-Reis <gustavodonisete11@gmail.com>
2025-10-21 21:47:32 -0300

    :construction: Salvando usuários em memória (Headers)

--- src/server.js
+++ src/server.js
@@ -1,15 +1,25 @@
 import http from 'node:http'
 
+const tasks = []
+
 const server = http.createServer((req, res) => {
   const { method, url } = req
 
   console.log(method, url)
 
   if(method === 'GET' && url === '/tasks') {
-    return res.end('Listagem de tarefas')
+    return res
+      .setHeader('Content-type', 'application/json')
+      .end(JSON.stringify(tasks))
   }
 
   if(method === 'POST' && url === '/tasks') {
+    tasks.push({
+      id: 1,
+      name: 'John Doe',
+      email: 'johndoe@example.com'
+    })
+
     return res.end('Criação de Tarefas')
   }
   
