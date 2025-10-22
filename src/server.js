7fe5e16779c9821f019503df927c54f05ade9aa9
GDC-Reis <gustavodonisete11@gmail.com>
2025-10-21 21:25:29 -0300

    :construction: Criando rotas GET e POST

--- src/server.js
+++ src/server.js
@@ -4,6 +4,14 @@ const server = http.createServer((req, res) => {
   const { method, url } = req
 
   console.log(method, url)
+
+  if(method === 'GET' && url === '/tasks') {
+    return res.end('Listagem de tarefas')
+  }
+
+  if(method === 'POST' && url === '/tasks') {
+    return res.end('Criação de Tarefas')
+  }
   
   return res.end('Hello World')
 })
